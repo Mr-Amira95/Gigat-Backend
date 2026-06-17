8d7ddfc6fdbfeae5f5fd44697fd6604c9921455e
hussam <hussam.amira95@gmail.com>
2026-06-17 16:09:34 +0300

    fix-web-login-response

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -139,7 +139,7 @@ class AuthController extends Controller
                 'token' => $result['token'],
             ]);
         } catch (Exception $e) {
-            return $this->exceptionResponse($e);
+            return $this->exceptionResponse($e, $e->getMessage());
         }
     }
 
